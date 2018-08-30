9ac48046459f6fb5dbb17ec50aaa72d195f4e86a
guotai.shen <guotai.shen@amlogic.com>
2018-08-30 14:47:12 +0800

    add file auto_patch.sh for auto patch function

---
+++ auto_patch.sh
@@ -0,0 +1,62 @@
+#opyright (C) 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+function auto_patch()
+{
+    T=$(pwd)
+    local patch_dir=$T/vendor/amlogic/tools/DroidlogicPatch
+    echo -e "patch_dir $patch_dir\n"
+
+    for file in $patch_dir/*
+    do
+        if [ -f "$file" ] && [ "${file##*.}" == "patch" ]
+        then
+            local file_name=${file%.*}
+            #echo file_name $file_name
+            local resFile=`basename $file_name`
+            #echo resFile $resFile
+            local dir_name1=${resFile//#/\/}
+            #echo dir_name $dir_name
+            local dir_name=${dir_name1%/*}
+            #echo dir_name $dir_name
+            local dir=$T/$dir_name
+            #echo $dir
+            local change_id=`grep 'Change-Id' $file | cut -f 2 -d " "`
+            if [ -d "$dir" ]
+            then
+                cd $dir; git log | grep $change_id 1>/dev/null 2>&1;
+                if [ $? -ne 0 ]; then
+                    echo -e "patch $file\n"
+                    cd $dir; git am $file;
+                    if [ $? != 0 ]
+                    then
+                        return 1
+                    fi
+                else
+             	      echo "has patched"
+                fi
+            fi
+            cd $T
+        fi
+    done
+}
+
+auto_patch
+
+if [ $? != 0 ]
+then
+    echo "patch error"
+    return 1
+fi
