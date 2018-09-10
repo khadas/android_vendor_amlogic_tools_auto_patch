8f937bf09e9de6880943c8ca91a67685edb71901
yu fang <yu.fang@amlogic.com>
2018-09-10 14:39:54 +0800

    Settings: fix Settings crash and update auto_patch.sh .[1/1]
    PD# 171673
    
    Change-Id: I464889c183286ba951add961dccda8effb65a6cf

--- auto_patch.sh
+++ auto_patch.sh
@@ -15,24 +15,18 @@
 
 function auto_patch()
 {
-    T=$(pwd)
-    local patch_dir=$T/vendor/amlogic/tools/DroidlogicPatch
+    local patch_dir=$1
     echo -e "patch_dir $patch_dir\n"
 
     for file in $patch_dir/*
     do
         if [ -f "$file" ] && [ "${file##*.}" == "patch" ]
         then
-            local file_name=${file%.*}
-            #echo file_name $file_name
-            local resFile=`basename $file_name`
-            #echo resFile $resFile
-            local dir_name1=${resFile//#/\/}
-            #echo dir_name $dir_name
-            local dir_name=${dir_name1%/*}
-            #echo dir_name $dir_name
-            local dir=$T/$dir_name
-            #echo $dir
+            local file_name=${file%.*};           #echo file_name $file_name
+            local resFile=`basename $file_name`;  #echo resFile $resFile
+            local dir_name1=${resFile//#/\/};     #echo dir_name $dir_name
+            local dir_name=${dir_name1%/*};       #echo dir_name $dir_name
+            local dir=$T/$dir_name;               #echo $dir
             local change_id=`grep 'Change-Id' $file | cut -f 2 -d " "`
             if [ -d "$dir" ]
             then
@@ -45,15 +39,30 @@ function auto_patch()
                         return 1
                     fi
                 else
-             	      echo "has patched"
+                    echo $file" has patched"
                 fi
             fi
-            cd $T
         fi
     done
 }
 
-auto_patch
+function traverse_patch_dir()
+{
+    T=$(pwd)
+    local local_dir=$T/vendor/amlogic/tools/auto_patch/
+    echo $local_dir
+    for file in `ls $local_dir`
+    do
+        if [ -d $local_dir$file ]
+        then
+            local dest_dir=$local_dir$file
+            auto_patch $dest_dir
+        fi
+    done
+    cd $T
+}
+
+traverse_patch_dir
 
 if [ $? != 0 ]
 then
